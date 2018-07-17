b9026bd22a1ad63353af43f92ac68558fa73e5df
Abhishek <dhanvantriabhi@gmail.com>
2018-07-17 23:31:41 +0530

    Login component

--- MicroSpas/Fruits-App/src/app/app.module.ts
+++ MicroSpas/Fruits-App/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material.module';
+import { FormsModule } from '../../node_modules/@angular/forms';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { LoginService } from './services/login.service';
+import { MainComponent } from './main/main.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppMaterialModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [ LoginService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
